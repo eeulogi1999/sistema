0a0a8abf140435c10561612a6da3e035a86eee52
eeulogi1999 <eeulogi1999@gmail.com>
2023-01-21 12:34:08 -0500

    add-sql
    add-sql

--- BD/updates.sql
+++ BD/updates.sql
@@ -1,25 +1,4 @@
 
-
-ALTER TABLE `colaboradores` ADD `col_ppagos` JSON NULL DEFAULT NULL AFTER `col_fechaIT`;
-
-CREATE TABLE `company5_bd_20602849172`.
-`sweeks`(`swe_id`
-    INT NOT NULL AUTO_INCREMENT, `swe_col_id`
-    BIGINT NOT NULL, `swe_week`
-    INT NOT NULL, `swe_saldo`
-    DECIMAL(12, 6) NOT NULL, PRIMARY KEY(`swe_id`)) ENGINE = InnoDB;
-
-
-    ALTER TABLE `sweeks` ADD CONSTRAINT `sweeks_colaborador` FOREIGN KEY (`swe_col_id`) REFERENCES `colaboradores`(`col_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
-
-    ALTER TABLE `colaboradores` CHANGE `col_ppagos` `col_hsbase` JSON NULL DEFAULT NULL;
-
-
-
-    ALTER TABLE `cuentas` ADD `cue_porcentaje` INT NULL AFTER `cue_gt4_id`;
-
-
     ALTER TABLE `ubicaciones` CHANGE `ubi_piso` `ubi_clave` VARCHAR(220) DEFAULT 'Nombre';
     ALTER TABLE `ubicaciones` CHANGE `ubi_etrabajo` `ubi_valor` VARCHAR(220) NOT NULL;
     ALTER TABLE `ubicaciones` ADD `ubi_ubi_id` BIGINT NULL AFTER `ubi_valor`;
