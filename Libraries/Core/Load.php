0287a4140e17ffc349f54e715b70b605e88b81a9
eeulogi1999 <eeulogi1999@gmail.com>
2023-03-14 19:17:47 -0500

    update 22

--- Libraries/Core/Load.php
+++ Libraries/Core/Load.php
@@ -1,12 +1,12 @@
 <?php 
 	$controller = ucwords($controller);
+	echo json_encode($_SERVER,JSON_UNESCAPED_UNICODE);
+	die();
 	$controllerFile = "Controllers/".$controller.".php";
 	if(file_exists($controllerFile))
 	{
 		require_once($controllerFile);
 		$controller = new $controller();
-		echo json_encode($_SERVER,JSON_UNESCAPED_UNICODE);
-		die();
 		if(method_exists($controller, $method))
 		{
 			$controller->{$method}($params);
