f285184101648c09e161eeffb9a0c884aac7d99e
eeulogi1999 <eeulogi1999@gmail.com>
2023-03-14 19:26:24 -0500

    spl 2

--- Libraries/Core/Autoload.php
+++ Libraries/Core/Autoload.php
@@ -1,5 +1,5 @@
 <?php 
-	echo json_encode(array(spl_autoload_register()),JSON_UNESCAPED_UNICODE);
+	echo json_encode(array($class),JSON_UNESCAPED_UNICODE);
 	die();
 	spl_autoload_register(function($class){
 		if(file_exists("Libraries/".'Core/'.$class.".php")){
