455b202609da6b281cf4253e164e00ece59c77ae
eeulogi1999 <eeulogi1999@gmail.com>
2023-03-14 19:53:10 -0500

    base-incomplete

--- Libraries/Core/Autoload.php
+++ Libraries/Core/Autoload.php
@@ -1,12 +1,12 @@
 <?php 
-	// spl_autoload_register(function($class){
-	// 	dep($class,false);
-	// 	if(file_exists("Libraries/".'Core/'.$class.".php")){
-	// 		require_once("Libraries/".'Core/'.$class.".php");
-	// 	}
-	// });
-	require_once("Libraries/Core/Controllers.php");
-	require_once("Libraries/Core/Views.php");
-	require_once("Libraries/Core/Mysql.php");
-	require_once("Libraries/Core/Conexion.php");
+	spl_autoload_register(function($class){
+		dep($class,false);
+		if(file_exists("Libraries/".'Core/'.$class.".php")){
+			require_once("Libraries/".'Core/'.$class.".php");
+		}
+	});
+	// require_once("Libraries/Core/Controllers.php");
+	// require_once("Libraries/Core/Views.php");
+	// require_once("Libraries/Core/Mysql.php");
+	// require_once("Libraries/Core/Conexion.php");
  ?>
