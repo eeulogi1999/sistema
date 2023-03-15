1f4ae153a2350b1e12268b8bd57b4daead225131
eeulogi1999 <eeulogi1999@gmail.com>
2023-03-14 19:49:46 -0500

    vies 2

--- Libraries/Core/Views.php
+++ Libraries/Core/Views.php
@@ -1,9 +1,9 @@
 <?php
-	echo json_encode('hola',JSON_UNESCAPED_UNICODE);
-	die();
 	class Views{
 		function getView($controller,$view,$data=""){
 			$controller = get_class($controller);
+			echo json_encode($controller,JSON_UNESCAPED_UNICODE);
+			die();
 			if($controller == "Home"){
 				$view = "Views/".$view.".php";
 			}else{
