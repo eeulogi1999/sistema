83ba7c87ef9ac4ca3511e751ccc26d79572237dc
eeulogi1999 <eeulogi1999@gmail.com>
2023-03-14 19:40:33 -0500

    ultolodas hide
    ultolodas hide

--- Libraries/Core/Autoload.php
+++ Libraries/Core/Autoload.php
@@ -1,9 +1,12 @@
 <?php 
-	echo json_encode(array($class),JSON_UNESCAPED_UNICODE);
-	die();
-	spl_autoload_register(function($class){
-		if(file_exists("Libraries/".'Core/'.$class.".php")){
-			require_once("Libraries/".'Core/'.$class.".php");
-		}
-	});
+	// spl_autoload_register(function($class){
+	// 	dep($class,false);
+	// 	if(file_exists("Libraries/".'Core/'.$class.".php")){
+	// 		require_once("Libraries/".'Core/'.$class.".php");
+	// 	}
+	// });
+	require_once("Libraries/Core/Controllers.php");
+	require_once("Libraries/Core/Views.php");
+	require_once("Libraries/Core/Mysql.php");
+	require_once("Libraries/Core/Conexion.php");
  ?>
