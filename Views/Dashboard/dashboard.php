8d196280255ae56d3c03afc39637f5d69b153390
eeulogi1999 <eeulogi1999@gmail.com>
2023-03-25 16:26:40 -0500

    Update dashboard.php

--- Views/Dashboard/dashboard.php
+++ Views/Dashboard/dashboard.php
@@ -22,6 +22,16 @@
     </div>
   </div>
   <div class="row">
+  <div class="col-md-12">
+      <div class="tile">
+        <div class="tile-body display-6">
+        Entre ustedes ni siquiera debe mencionarse la inmoralidad sexual, ni ninguna clase de impureza o de avaricia, porque eso no es propio del pueblo santo de Dios.
+Efesios 5:3
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- <div class="row">
     <div class="col-md-12">
       <div class="tile">
         <div class="tile-body">
@@ -31,56 +41,10 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
   <div class="row">
-    <div class="col-md-4">
-      <div class="tile">
-        <div class="tile-body">
-          <!-- <h5 class="text-center">NOTAS</h5> -->
-          <!-- PRECIOS ACTUALIZADOS 
-            Brillo 31.50
-            Grueso 30
-            Delgado 29
-            Grupo 5.10 
-            Bronce 19.50
-            Rad.bronce 17
-            Viruta de bronce 14
-            Duro 5.40
-            Perfil 6.20
-            Acero 4
-            Rad. Aluminio 4
-            Olla sucia 4.50
-            Olla limpia 5.50
-            Oxee 6
-            Batería 67
-            Chata 57
-            Tierra bateria 3.90
-            Cocalata 3.80
-            Cable 6.50
-            Cable grasa 6
-            Cable sulfatado 5.5
-            Carrocería 4.50
-            Antimonio 8
-            Chapa 2 -->
-          <figure class="highcharts-figure">
-            <div id="ten_copper"></div>
-            <p class="highcharts-description"></p>
-          </figure>
-        </div>
-      </div>
-    </div>
-    <!-- <div class="col-md-4">
-      <div class="tile">
-        <div class="tile-body">
-          <figure class="highcharts-figure">
-            <div id="ten_plomo"></div>
-            <p class="highcharts-description"></p>
-          </figure>
-        </div>
-      </div>
-    </div> -->
     <?php if ($_SESSION['gcl']['gcl_gem_id']['gem_ruc'] == '20609315327') { ?>
-    <div class="col-md-8">
+    <div class="col">
       <div class="tile">
         <div class="tile-body">
           <h4 class="text-center">CUENTAS</h4>
@@ -131,7 +95,7 @@
     </div>
     <?php  } ?>
     <?php if ($_SESSION['gcl']['gcl_gem_id']['gem_ruc'] == '20608036483') { ?>
-    <div class="col-md-8">
+    <div class="col">
       <div class="tile">
         <div class="tile-body">
           <h4 class="text-center">CUENTAS</h4>
@@ -167,9 +131,7 @@
       </div>
     </div>
     <?php  } ?>
-  </div>
-  <div class="row">
-    <div class="col-md-12">
+    <div class="col">
       <div class="tile">
         <div class="tile-body">
           <h4>NOTICIAS</h4>
