c11d02a9ad1f50022c0d86d227d00ba5e617dee4
eeulogi1999 <eeulogi1999@gmail.com>
2023-07-12 11:12:14 -0500

    php obs pdf 1

--- Controllers/Movimientos.php
+++ Controllers/Movimientos.php
@@ -1,5 +1,4 @@
 <?php 
-@ob_start();
 require 'Libraries/html2pdf/vendor/autoload.php';
 require 'Libraries/numero2letras/vendor/autoload.php';
 require_once 'Libraries/dompdf/vendor/autoload.php';
@@ -474,7 +473,7 @@ class Movimientos extends Controllers{
         die();
     }
     public function getPdf($mov_id){
-        ob_end_clean();
+        //ob_end_clean();
         $result = Endroid\QrCode\Builder\Builder::create()
             ->writer(new Endroid\QrCode\Writer\PngWriter())
             ->writerOptions([])
@@ -528,7 +527,7 @@ class Movimientos extends Controllers{
         die();
     }
     public function getHtml($mov_id){
-        ob_end_clean();
+        //ob_end_clean();
         $result = Endroid\QrCode\Builder\Builder::create()
             ->writer(new Endroid\QrCode\Writer\PngWriter())
             ->writerOptions([])
@@ -610,7 +609,6 @@ class Movimientos extends Controllers{
         echo json_encode($mde,JSON_UNESCAPED_UNICODE);
         die();  
     }
-    
-
 }
+//@ob_start('getPdf');
 ?>
